13345d505027a710bb0d4a113c007e71ad9c0dc7
unknown <simon@adision.ca>
2026-05-04 18:36:54 -0400

    cors wildcard

--- api.py
+++ api.py
@@ -10,19 +10,9 @@ app = FastAPI(title="Adision API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-        "http://localhost:5175",
-        "http://127.0.0.1:5175",
-        "http://localhost:5176",
-        "http://127.0.0.1:5176",
-        "https://spinach-monitor-deflation.ngrok-free.app",
-        "https://ad-budget-client.vercel.app",
+    allow_origins=["*"],
         
-    ],
+   
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
